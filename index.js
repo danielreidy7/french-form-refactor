48ac06f3fe94ff4cb9851b45fbf812eb3f95c874
danielreidy7 <danielconnorreidy@gmail.com>
2020-11-30 11:59:29 +0100

    added some vanilla js to autofill time and date inputs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+
+window.onload= function() {
+  let date = new Date();
+  let currentDate = date.toISOString().slice(0,10);
+  let currentTime = date.getHours() + ':' + date.getMinutes();
+  document.querySelector('.set-today').value=currentDate;
+  document.querySelector('.set-now').value=currentTime;
+}
+
+
 let checker = (check) => {
   check.classList.toggle("checked");
-};
-
-let addSlashes = (input) => {
-  console.log(input.value)
-};
-
-let addColon = (input) => {
-  console.log(input.value)
 };
