ac676e4be0fad8f5f1a2af7e79d2aff5d101cf6f
danielreidy7 <danielconnorreidy@gmail.com>
2020-11-30 12:16:22 +0100

    Algolia auto search for the address field

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 window.onload= function() {
   let date = new Date();
   let currentDate = date.toISOString().slice(0,10);
-  let currentTime = date.getHours() + ':' + date.getMinutes();
+  var currentTime = moment(date).format('hh:mm');
   document.querySelector('.set-today').value=currentDate;
   document.querySelector('.set-now').value=currentTime;
 }
+
+const placesAutocomplete = places({
+  appId: 'plEQCR37KMQ9',
+  apiKey: '5701b721593618ffd6409a49f9b11747',
+  container: document.querySelector('#address-input')
+});
 
 
 let checker = (check) => {
