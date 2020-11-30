df8ce4bb49065b053d9ae79a2afd51d73d5cca69
danielreidy7 <danielconnorreidy@gmail.com>
2020-11-30 11:39:59 +0100

    less is more. Simplified styling, worked on white space. Animation.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 let checker = (check) => {
   check.classList.toggle("checked");
 };
+
+let addSlashes = (input) => {
+  console.log(input.value)
+};
+
+let addColon = (input) => {
+  console.log(input.value)
+};
